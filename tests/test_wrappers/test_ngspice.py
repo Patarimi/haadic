617a734fd5c7fc30b3547baaa933ea9cdbbd58fd
Patarimi <38954040+Patarimi@users.noreply.github.com>
2025-08-10 11:41:22 +0200

    avoid rewrite schematic file in test. Remove line return eronous addition.

--- tests/test_wrappers/test_ngspice.py
+++ tests/test_wrappers/test_ngspice.py
@@ -1,16 +1,19 @@
 import fileinput
-import pathlib
+from pathlib import Path
 from filecmp import cmp
+import shutil
 
 from hades.wrappers.ngspice import NGSpice
 
 
 def test_ngspice(tmp_path):
     spice = NGSpice()
+    spice_file = tmp_path / "schem_test.net"
     data_file = tmp_path / "out.raw"
-    spice.compute(pathlib.Path("./tests/test_wrappers/schem_test.net"), data_file)
-    assert pathlib.Path(data_file).exists()
-    assert pathlib.Path(tmp_path / "out.log").exists()
+    shutil.copy("./tests/test_wrappers/schem_test.net", spice_file)
+    spice.compute(spice_file, data_file)
+    assert Path(data_file).exists()
+    assert Path(tmp_path / "out.log").exists()
     # remove the line with the date before comparison
     for line in fileinput.input(data_file, inplace=True):
         if not line.startswith("Date:") and not line.startswith("Command:"):
