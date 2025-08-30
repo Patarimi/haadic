19e8430ac8fbbdf0528eebf949f28fb54442219c
Patarimi <mpqqch@gmail.com>
2025-08-30 19:23:40 +0200

    templateV1

---
+++ hades/template/{{cookiecutter.project_name}}/design.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from hades.layouts.tools import LayerStack
+
+
+techno = "{{ cookiecutter.techno_name }}"
+target: dict[str, float] = {}
+
+def local_model(target: dict[str, float]) -> dict[str, float]:
+    return {}
+
+def layout(cell, layerstack: LayerStack, **kwargs):
+    pass
+
+def evaluate(bench_data: pd.DataFrame) -> dict[str, float]:
+    return {}
