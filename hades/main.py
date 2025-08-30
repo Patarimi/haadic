b6bab93737d15f2b0f7721f6e442fa088ddbd96c
Patarimi <mpqqch@gmail.com>
2025-08-30 20:05:10 +0200

    remove generate remove unused poe command

--- hades/main.py
+++ hades/main.py
@@ -6,15 +6,9 @@ from cyclopts import App
 from pathlib import Path
 
 from tabulate import tabulate
-from hades.devices.mos import Mos
-from hades.devices.inductor import Inductor
-from hades.devices.micro_strip import MicroStrip
-from hades.devices.device import generate, Step
 from hades.layouts.tools import check_diff
-import yaml
 from os.path import join
 import hades.techno as techno
-import hades.wrappers.simulator as sim
 
 if not logging.getLogger().hasHandlers():
     logging.basicConfig(
@@ -28,29 +22,6 @@ if not logging.getLogger().hasHandlers():
 
 app = App()
 app.command(techno.pkd_app)
-app.command(sim.sim_app)
-if shutil.which("openEMS"):
-    from hades.wrappers.oems import oems_app
-
-    app.command(oems_app)
-
-
-@app.command(name="generate")
-def generate_cli(design_yaml: Path = Path("./design.yml"), stop: str = "full") -> None:
-    """Main command. Run the flow until convergence using _design.yaml_. The design can be stopped at a specific step using the _stop_ option."""
-    with open(design_yaml) as f:
-        conf = yaml.load(f, Loader=yaml.Loader)
-    design = conf["design"]
-    if design["device"] == "mos":
-        dut = Mos()
-    elif design["device"] == "inductor":
-        dut = Inductor(name=conf["name"], techno=conf["techno"])
-    elif design["device"] == "micro-strip":
-        dut = MicroStrip(name=conf["name"], techno=conf["techno"])
-    else:
-        raise RuntimeError("Unknown device, choice are mos, inductor")
-    dimensions = design["dimensions"]
-    generate(dut, design["specifications"], dimensions, Step[stop])  #  type: ignore[invalid-argument-type]
 
 
 @app.command(name="run")
