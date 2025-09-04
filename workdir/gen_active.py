22ac3eb8ca38cdf81b9a4c05cc43c6c7ed0110ff
Patarimi <mpqqch@gmail.com>
2025-09-04 14:03:52 +0200

    add some optimisation for better fit.

--- workdir/gen_active.py
+++ workdir/gen_active.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
+import scipy
 from hades.layouts.active import mosfet, line, connect
 from hades.layouts.general import set_as_port
 from hades.layouts.tools import LayerStack
@@ -70,20 +71,76 @@ def evaluate(bench_data: pd.DataFrame, dis_plot: bool = False) -> dict[str, floa
         lbda_c = 0
         asymptote = 1 / np.sqrt(IC)
         lbl = r"$1/\sqrt{i_d}$"
-    params = {"n": n, "i_spec": i_spec, "lbda_c": lbda_c, "L": model["length"],
-              "id": np.max(id)*1e3, "IC": np.max(IC)}
+    params = {
+        "n": n,
+        "i_spec": i_spec,
+        "lbda_c": lbda_c,
+        "L": model["length"],
+        "id": np.max(id) * 1e3,
+        "IC": np.max(IC),
+    }
     with open("model.json", "r") as f:
         model = json.load(f)
-    with open("model.json", "w") as f:
-        for key in params:
-            model[key] = params[key]
-        json.dump(model, f, indent=2)
     if dis_plot:
-        plt.loglog(IC, n * gm * ut / id, label="$(n g_m U_t)/i_d$")
-        plt.hlines(1, np.min(IC), np.max(IC), linestyles="dashed", label="1")
-        plt.loglog(IC, asymptote, label=lbl, linestyle="dashed")
-        plt.ylim(top=2)
-        plt.xlabel("IC (-)")
+        gm_IC_simu = gm * ut / id
+
+        def gm_ekv(IC, l, n):
+            return (np.sqrt((l * IC + 1) ** 2 + 4 * IC) - 1) / (
+                IC * (l * (l * IC + 1) + 2) * n
+            )
+
+        def error(x, y):
+            return 100 * np.abs(x - y) / x
+
+        gm_IC = gm_ekv(IC, lbda_c, n)
+        crop = 60
+        lmb_opt = scipy.optimize.curve_fit(
+            gm_ekv, IC[crop:], gm_IC_simu[crop:], p0=[lbda_c, n]
+        )
+        _, ax = plt.subplots(2, 1, sharex=True)
+        ax[0].loglog(IC, gm_IC_simu, label="$(n g_m U_t)/i_d$")
+        # plt.hlines(1, np.min(IC), np.max(IC), linestyles="dashed", label="1")
+        # plt.loglog(IC, asymptote, label=lbl, linestyle="dashed")
+        ax[0].loglog(
+            IC,
+            gm_IC,
+            label="model (first guess)",
+            linestyle="dotted",
+            linewidth=2,
+            color="C2",
+        )
+        ax[0].loglog(
+            IC,
+            gm_ekv(IC, *(lmb_opt[0])),
+            label="model (opt. fit)",
+            linestyle="dotted",
+            color="C3",
+        )
+        ax[0].set_ylim(top=2)
+        ax[1].set_xlabel("IC (-)")
+        ax[0].legend()
+        ax[1].plot(
+            IC,
+            error(gm_IC_simu, gm_IC),
+            linestyle="dashed",
+            color="C2",
+            label="Error (first guess)",
+        )
+        ax[1].plot(
+            IC,
+            error(gm_IC_simu, gm_ekv(IC, *(lmb_opt[0]))),
+            linestyle="dashed",
+            color="C3",
+            label="Error (opt. fit)",
+        )
         plt.legend()
+        ax[1].set_ylabel("Error (%)")
+        ax[1].set_ylim(0, 30)
         plt.show()
+        params["lbda_c_opt"] = lmb_opt[0][0]
+        params["n_opt"] = lmb_opt[0][1]
+        with open("model.json", "w") as f:
+            for key in params:
+                model[key] = params[key]
+            json.dump(model, f, indent=2)
     return params
