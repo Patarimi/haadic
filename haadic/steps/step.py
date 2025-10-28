b8c2e770dd17043a536ec5a3680bdc3d4d1730f9
Patarimi <38954040+Patarimi@users.noreply.github.com>
2025-10-28 15:58:29 +0100

    add libspice and new section key

--- haadic/steps/step.py
+++ haadic/steps/step.py
@@ -14,7 +14,7 @@ from haadic.layouts.tools import LayerStack
 from haadic.extractors.spicing import ExtractOptions, extract_spice_magic
 from haadic.parsers.raw import parse_out
 from haadic.wrappers.ngspice import compute
-from haadic.techno import get_file
+from haadic.techno import get_file, load_pdk
 from haadic.parsers.netlist import Netlist
 from haadic.wrappers.tools import to_wsl
 
@@ -111,7 +111,8 @@ def run_bench(bench_name: Path | str = "bench.cir", techno: str = "sky130"):
         if oth.startswith(".lib") and to_wsl(get_file(techno, "lib_spice")) in oth:
             skip_lib_add = True
     if not skip_lib_add:
-        spice.add_other(f".lib {to_wsl(get_file(techno, 'lib_spice'))} tt")
+        section = "tt" if "section" not in load_pdk(techno) else load_pdk(techno)["section"]
+        spice.add_other(f".lib {to_wsl(get_file(techno, 'lib_spice'))} {section}")
     spice.write(bench_name)
 
     compute(Path(bench_name), data_file)
