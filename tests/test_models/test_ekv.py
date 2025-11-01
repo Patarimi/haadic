5be0acadc6b2b8bee74dab65d0f41ccd33d87b50
Patarimi <mpqqch@gmail.com>
2025-11-01 19:42:03 +0100

    fix the fix

--- tests/test_models/test_ekv.py
+++ tests/test_models/test_ekv.py
@@ -35,4 +35,4 @@ def test_ekv_model(tmp_path):
 def test_ekv_sky130():
     ekv = EKV("sky130")
     assert ekv.length == 0.18
-    assert pytest.approx(ekv.n, abs=1e-3) == 1.51
+    assert pytest.approx(ekv.n, abs=1e-3) == 1.501
