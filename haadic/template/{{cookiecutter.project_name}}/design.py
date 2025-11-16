ba0000bfb54b68112a5045e47fd5b983c08ca500
Patarimi <mpqqch@gmail.com>
2025-11-16 14:59:26 +0100

    update template from project creation

--- haadic/template/{{cookiecutter.project_name}}/design.py
+++ haadic/template/{{cookiecutter.project_name}}/design.py
@@ -1,20 +1,22 @@
 import pandas as pd
 from haadic.layouts.tools import LayerStack
+from haadic.steps.step import Dim
+from klayout.db import Cell
 
 
 techno = "{{ cookiecutter.techno_name }}"
-target: dict[str, float] = {}
+target: Dim = Dim({})
 
 benches = ("bench.cir",)
 
 
-def local_model(target: dict[str, float]) -> dict[str, float]:
-    return {}
+def local_model(target: Dim) -> Dim:
+    return Dim({})
 
 
-def layout(cell, layerstack: LayerStack, **kwargs):
+def layout(cell: Cell, layerstack: LayerStack, Dim):
     pass
 
 
-def evaluate(bench_data: pd.DataFrame) -> dict[str, float]:
-    return {}
+def evaluate(bench_data: pd.DataFrame) -> Dim:
+    return Dim({})
