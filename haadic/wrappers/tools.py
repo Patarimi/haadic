b9ce65800361ec6b3c0890d26a67787bcf575ce9
Patarimi <38954040+Patarimi@users.noreply.github.com>
2025-11-18 15:24:23 +0000

    miss a semi-colon

--- haadic/wrappers/tools.py
+++ haadic/wrappers/tools.py
@@ -48,7 +48,7 @@ def nix_run(cmd: list[str]) -> CompletedProcess:
     ]
     if os.name == "nt":
         over_head = [
-            "$env:WSLENV+= 'PDK_ROOT/up'",  # propagate PDK_ROOT to WSL
+            "$env:WSLENV+= 'PDK_ROOT/up';",  # propagate PDK_ROOT to WSL
             "wsl",
             "-d",
             "NixOS",
