a595b307e9b0520944d41b35cd5176ac4aeca594
Patarimi <mpqqch@gmail.com>
2025-11-30 17:50:52 +0100

    format

--- tests/test_parser/test_tlef.py
+++ tests/test_parser/test_tlef.py
@@ -4,24 +4,36 @@ from haadic.techno import get_file, is_installed
 
 pytestmark = pytest.mark.skipif((not is_installed("mock")), reason="PDK not installed.")
 
+
 @pytest.fixture
 def mock_layer_stack():
     return get_file("mock", "techlef")
 
-@pytest.mark.parametrize("nbr, expected_layer", [
-    (0, Layer(name="Metal1", type="ROUTING", width=0.4, spacing=0)),
-    (-1, Layer(name="Pad", type="ROUTING")),])
+
+@pytest.mark.parametrize(
+    "nbr, expected_layer",
+    [
+        (0, Layer(name="Metal1", type="ROUTING", width=0.4, spacing=0)),
+        (-1, Layer(name="Pad", type="ROUTING")),
+    ],
+)
 def test_load_tlef(nbr, expected_layer, mock_layer_stack):
     path = mock_layer_stack
     assert get_metal(nbr, path) == expected_layer
 
-@pytest.mark.parametrize("nbr, expected_layer", [
-    (1, Layer(name="Via1", type="CUT", width=0.4, enclosure=0.2, spacing=0.26)),
-    (2, Layer(name="Via2", type="CUT", width=0.4, enclosure=0.2, spacing=0.26)),
-    (-1, Layer("Opening", "CUT", )),])
+
+@pytest.mark.parametrize(
+    "nbr, expected_layer",
+    [
+        (1, Layer(name="Via1", type="CUT", width=0.4, enclosure=0.2, spacing=0.26)),
+        (2, Layer(name="Via2", type="CUT", width=0.4, enclosure=0.2, spacing=0.26)),
+        (-1, Layer(name="Opening", type="CUT")),
+    ],
+)
 def test_get_via(nbr, expected_layer, mock_layer_stack):
     path = mock_layer_stack
     assert get_via(nbr, path) == expected_layer
+
 
 def test_load_tlef_properties(mock_layer_stack):
     path = mock_layer_stack
