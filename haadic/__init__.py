e0c60a27ee86d37d5f458f041afadfca980cd817
Patarimi <mpqqch@gmail.com>
2026-04-12 16:56:30 +0200

    Better doc

--- haadic/__init__.py
+++ haadic/__init__.py
@@ -1,1 +1,15 @@
 name = "haadic"
+
+"""
+Haadic structure:
+ - core
+ - io
+    - readers: files readers (spice, raw, etc.)
+    - writers: files writers (spice)
+    - wrappers: for external tools (e.g., magic, netgen, etc.)
+ - design
+    - layouts: for layout generation and manipulation
+    - models: for device and circuit modeling
+    - evaluators: for performance evaluation
+    - components: 
+"""
