f2c57c572ee4f747fef93607378530ca67a27f1c
Patarimi <38954040+Patarimi@users.noreply.github.com>
2026-04-20 11:39:01 +0200

    doc improvment

--- haadic/core/techno.py
+++ haadic/core/techno.py
@@ -29,7 +29,10 @@ PDK_INSTALL_DIR = Path(
 
 @pkd_app.command(name="install")
 def install(pdk_name: Available_PDK):
-    """Install the _pdk_name_ technology in its default location."""
+    """Install the _pdk_name_ technology in its default location.
+
+    :param Available_PDK pdk_name: name of the PDK to be installed.
+    """
     base_install = Path(PDK_INSTALL_DIR)
     tech = load_pdk(pdk_name)
     base_url = tech["source_url"]
@@ -153,6 +156,12 @@ def add_reference(
 
 
 def get_file(pdk_name: str, file_type: str) -> Path:
+    """Utility function to get a configuration file for a given technologie.
+
+    :param str pdk_name: selected technologie (from AvailablePDK).
+    :param str file_type: software configuration files.
+    :return Path: path to the requested files.
+    """
     pdk = load_pdk(pdk_name)
     if file_type == "base_dir":
         if pdk_name == "mock":
