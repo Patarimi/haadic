eac270e2c3bbb1ed3546457538bb717bef188598
Patarimi <mpqqch@gmail.com>
2026-05-25 15:18:06 +0200

    last missing docstring

--- haadic/__init__.py
+++ haadic/__init__.py
@@ -1,8 +1,11 @@
-name = "haadic"
-
 """
+Haadic: A Python framework for analog IC design automation.
+
 Haadic structure:
  - core
+   - flow: for defining and running design flows
+   - steps: for defining the core steps in the design flow, such as layout generation, extraction, simulation, etc.
+   - techno: for handling technology-specific information and operations.
  - io
     - readers: files readers (spice, raw, etc.)
     - writers: files writers (spice)
@@ -13,3 +16,5 @@ Haadic structure:
     - evaluators: for performance evaluation
     - components: parameterized components (e.g., common sources) and design helpers (e.g., EKV model).
 """
+
+name = "haadic"
