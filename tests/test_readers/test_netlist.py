545c8983f6751ee9aef318f6b40a5897309420cd
Patarimi <mpqqch@gmail.com>
2026-05-30 12:36:45 +0200

    improve netlist writer with reading capabilities move module in handlers

--- tests/test_readers/test_netlist.py
+++ tests/test_readers/test_netlist.py
@@ -1,20 +1,20 @@
+from difflib import unified_diff
 from haadic.io.wrappers.tools import to_wsl
-import filecmp
 
 from haadic._config import REF_PATH
-from haadic.io.writers.netlist import Component, Netlist
+from haadic.io.handlers.netlist import Component, Netlist
 
 
 def test_component():
-    c5 = Component("C", "5", 5e-12, ("gnd", "5"))
-    r_mid = Component("R", "mid", 5e3, ("5", "6"))
+    c5 = Component("C5", "gnd", "5", "5e-12")
+    r_mid = Component("Rmid", "5", "6", "5e3")
     assert str(c5) == "C5 gnd 5 5.000 pF"
     assert str(r_mid) == "Rmid 5 6 5.000 kΩ"
 
 
 def test_netlist(tmp_path):
     net = Netlist("test")
-    net.add_component(Component("C", "5", 5e-12, ("gnd", "5")))
+    net.add_component(Component("C5", "gnd", "5", "5e-12"))
     assert net.name == "test"
     assert net.spice() == "* test\nC5 gnd 5 5.000 pF\n"
     net.add_control("run")
@@ -29,8 +29,15 @@ run
 .endc
 """
     assert net.spice() == expected_spice
+
+def test_netlist_load(tmp_path):
     ref_spice = REF_PATH / "ref_netlist.spice"
     res_path = tmp_path / "test_output.spice"
     net = Netlist().load(ref_spice)
     net.write(res_path)
-    assert filecmp.cmp(res_path, ref_spice)
+    with open(res_path, "r") as f:
+        result = f.readlines()
+    with open(ref_spice, "r") as f:
+        reference = f.readlines()
+    diff = list(unified_diff(result, reference))
+    assert diff == [], f"Netlist loading failed. Differences:\n{''.join(diff)}"
