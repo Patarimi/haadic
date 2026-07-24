ddc647d3f65630a102e6a15a0b213ef2dd4abd60
Patarimi <mpqqch@gmail.com>
2026-07-24 19:33:17 +0200

    last one

--- tests/tests_io/test_wrappers/test_ngspice.py
+++ tests/tests_io/test_wrappers/test_ngspice.py
@@ -19,9 +19,10 @@ def test_ngspice(tmp_path):
     assert Path(data_file).exists()
     assert Path(spice_file.with_suffix(".log")).exists()
     # remove the line with the date before comparison
-    for line in fileinput.input(data_file, inplace=True):
-        if not line.startswith("Date:") and not line.startswith("Command:"):
-            print(line, end="")
+    with fileinput.input(data_file, inplace=True) as f:
+        for line in f:
+            if not line.startswith("Date:") and not line.startswith("Command:"):
+                print(line, end="")
     assert cmp(REF_PATH / "inv.raw", data_file)
 
     with pytest.raises(RuntimeError):
