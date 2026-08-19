15e68c4344c1d599bca4a53a7d7145b30324798f
Patarimi <mpqqch@gmail.com>
2026-08-19 19:51:13 +0200

    move data_write outside of compute function

--- haadic/io/wrappers/ngspice.py
+++ haadic/io/wrappers/ngspice.py
@@ -12,44 +12,30 @@ CURRENT_DIR = Path(__file__).parent
 
 def compute(
     input_file: Path,
-    data_file: Path | None = None,
     log_file: Path | None = None,
 ) -> None:
     """
     Simulate the spice input file with ngspice.
 
     :param input_file: a path to the spice input file.
-    :param data_file: a path to the data file. (default: same as input_file with .raw extension)
-    :param log_file: a path to the log file. (default: same as data_file with .log extension)
+    :param log_file: a path to the log file. (default: same as input_file with .log extension)
     :return: None
     """
-    if data_file is None:
-        data_file = Path(input_file).with_suffix(".raw")
     if log_file is None:
-        log_file = data_file.with_suffix(".log")
+        log_file = input_file.with_suffix(".log")
 
     if not nix_check():
         raise RuntimeError("nix is not installed.")
 
     # find the write statement and change the output file
-    write_edited = False
     filetype_edited = False
     with FileInput(files=(input_file), inplace=True) as circuit_file:
         for line in circuit_file:
-            if line.startswith("write"):
-                words = line.split(" ")
-                words[1] = to_wsl(data_file)
-                line = " ".join(words)
-                write_edited = True
             if line.startswith("set filetype"):
                 line = "set filetype = ASCII\n"
                 filetype_edited = True
-            if line.startswith(".endc"):
-                if not write_edited:
-                    # no write in the file, adding one
-                    print(f"write {data_file} all")
-                if not filetype_edited:
-                    print("set filetype = ASCII")
+            if line.startswith(".endc") and not filetype_edited:
+                print("set filetype = ASCII")
             print(line, end="")
 
     cmd = [
